ae8f18190ba798de8a9b7b03e796941c89c2b094
Nikodem Mazur <nikodem.mazur@gmail.com>
2021-01-02 13:34:55 +0100

    Rename namespace

--- App/Pages/Error.cshtml.cs
+++ App/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace App.Pages
+namespace Squidy.App.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
